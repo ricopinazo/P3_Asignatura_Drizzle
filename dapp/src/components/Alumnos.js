9f86eb5f275f06eef924f1b57d635778a3d77bb2
Santiago Pavon <santiago@dit.upm.es>
2020-12-03 16:57:26 +0100

    Añadido Drizzle y componentes vacios.

---
+++ dapp/src/components/Alumnos.js
@@ -0,0 +1,10 @@
+
+const Alumnos = () => (
+    <header className="AppAlumnos">
+        <h2>
+            Alumnos
+        </h2>
+    </header>
+);
+
+export default Alumnos;
---
+++ dapp/src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => (
+    <header className="AppHeader">
+        <h1>
+            P3 - Asignatura
+        </h1>
+    </header>
+);
+
+export default Header;
