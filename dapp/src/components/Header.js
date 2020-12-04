39f24e3a967debd5d0afeead78724973eb5a8026
Santiago Pavon <santiago@dit.upm.es>
2020-12-04 08:31:46 +0100

    Añadir componentes DrizzleReact a Header.

--- dapp/src/components/Header.js
+++ dapp/src/components/Header.js
@@ -1,8 +1,26 @@
+import {newContextComponents} from "@drizzle/react-components";
+const {ContractData} = newContextComponents;
 
-const Header = () => (
+const Header = props => (
     <header className="AppHeader">
         <h1>
-            P3 - Asignatura
+            P3 - Asignatura: &nbsp;
+            <ContractData
+                drizzle={props.drizzle}
+                drizzleState={props.drizzleState}
+                contract={"Asignatura"}
+                method={"nombre"}
+            />
+            -
+            <ContractData
+                drizzle={props.drizzle}
+                drizzleState={props.drizzleState}
+                contract={"Asignatura"}
+                method={"curso"}
+                render={data => (
+                    <em>{data}</em>
+                )}
+            />
         </h1>
     </header>
 );
